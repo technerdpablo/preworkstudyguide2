fb20bf5e2d698df4beb41c8687379f679386e8ed
pabloantonio <emailpabloroman@gmail.com>
2024-03-05 18:14:11 -0500

    added these nuts to JS

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+const topic = "HTML";
+
+
+/*
+if (topic === "HTML") {
+  console.log("lets study HTML");
+} else if (topic === "CSS") {
+  console.log("lets study CSS");
+} else if (topic === "JavaScript") {
+  console.log("lets study JavaScript");
+} else if (topic === "Git") {
+  console.log("lets study Git");
+}  else ('Please try again!');
+*/
+
+
+const topics = ["these", "nuts", "on", "your", "chin", "lol"];
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
